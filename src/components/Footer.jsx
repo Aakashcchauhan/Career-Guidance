e19129985267e6e29807eac79451daeaff71ced6
Aakash Chuahan <130996799+Aakashcchauhan@users.noreply.github.com>
2025-06-21 11:47:53 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
   <footer className=" bg-amber-50 dark:bg-gray-900 text-white text-center py-8 px-4 mt-12 border-t border-slate-700 dark:border-gray-700">
     <p>© 2025 OwnWay - Career Hub | All Rights Reserved</p>
-    <p className="text-slate-400 dark:text-slate-300 mt-2">Helping candidates ace technical interviews since 2023</p>
+    <p className="text-slate-400 dark:text-slate-300 mt-2">Helping candidates ace technical interviews since 2025</p>
   </footer>
   )
 }
